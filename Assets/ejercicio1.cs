051c4270aee6c00a77d143fc169532335c248c5c
valentinocornejo <101400293+valentinocornejo@users.noreply.github.com>
2022-04-07 15:09:48 -0300

    Ejercicio resuelto

---
+++ Assets/ejercicio1.cs
@@ -0,0 +1,28 @@
+﻿//Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 == num2)
+        {
+            Debug.Log("Las variables son iguales");
+        }
+        else
+        {
+            Debug.Log("Las variables no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ejercicio2.cs
@@ -0,0 +1,38 @@
+﻿//Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+//Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio2 : MonoBehaviour
+{
+    public float producto1;
+    public float producto2;
+    public float producto3;
+    public float monto;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        float suma;
+        float resto;
+        suma = producto1 + producto2 + producto3;
+
+        if(monto >= suma)
+        {
+            resto = monto - suma;
+            Debug.Log("Te sobro" + resto);
+        }
+        else
+        {
+            resto = suma - monto;
+            Debug.Log("Te falto" + resto);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
